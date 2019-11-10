6aadaf4235b262a09801ea028496f911e571e20c
rizal140600 <rizal14ali06@gmail.com>
2019-11-10 19:44:20 +0700

    lanjut frontend

--- routes/web.php
+++ routes/web.php
@@ -93,5 +93,5 @@ Route::post('/profil/struktur/create', 'StrukturController@create');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'HomeController@index')->name('home');
 
