b9723c6f6661c4b54c967b132a0abb88f7764bcd
rizal140600 <rizal14ali06@gmail.com>
2019-11-16 00:21:05 +0700

    komit

--- routes/web.php
+++ routes/web.php
@@ -13,99 +13,93 @@
 
 Route::get('/', 'Frontend\IndexController@index');
 
-Route::group(['middleware' => ['auth']], function () {
-  Route::get('/dashboard','AdminController@dashboard');
-  
-  Route::get('/backend/pembelajaran', 'PembelajaranController@index');
-  Route::post('/backend/pembelajaran/create', 'PembelajaranController@create');
-  Route::get('/backend/pembelajaran/{id}/edit', 'PembelajaranController@edit');
-  Route::post('/backend/pembelajaran/{id}/update', 'PembelajaranController@update');
-  Route::get('/backend/pembelajaran/{id}/delete', 'PembelajaranController@delete');
-  
-  Route::get('/backend/pengumuman', 'PengumumanController@index');
-  Route::post('/backend/pengumuman/create', 'PengumumanController@create');
-  Route::get('/backend/pengumuman/{id}/edit', 'PengumumanController@edit');
-  Route::post('/backend/pengumuman/{id}/update', 'PengumumanController@update');
-  Route::get('/backend/pengumuman/{id}/delete', 'PengumumanController@delete');
-  
-  Route::get('/backend/kegiatan', 'KegiatanController@index');
-  Route::post('/backend/kegiatan/create', 'KegiatanController@create');
-  Route::get('/backend/kegiatan/{id}/edit', 'KegiatanController@edit');
-  Route::post('/backend/kegiatan/{id}/update', 'KegiatanController@update');
-  Route::get('/backend/kegiatan/{id}/delete', 'KegiatanController@delete');
-  
-  Route::get('/backend/galeri', 'GaleriController@index');
-  Route::post('/backend/galeri/create', 'GaleriController@create');
-  Route::get('/backend/galeri/{id}/edit', 'GaleriController@edit');
-  Route::post('/backend/galeri/{id}/update', 'GaleriController@update');
-  Route::get('/backend/galeri/{id}/delete', 'GaleriController@delete');
-  
-  Route::get('/backend/profil/sarana', 'SaranaController@index');
-  Route::post('/backend/profil/sarana/create', 'SaranaController@create');
-  Route::get('/backend/profil/sarana/{id}/edit', 'SaranaController@edit');
-  Route::post('/backend/profil/sarana/{id}/update', 'SaranaController@update');
-  Route::get('/backend/profil/sarana/{id}/delete', 'SaranaController@delete');
-  
-  Route::get('/backend/guru', 'GuruController@index');
-  Route::post('/backend/guru/create', 'GuruController@create');
-  Route::get('/backend/guru/{id}/edit', 'GuruController@edit');
-  Route::post('/backend/guru/{id}/update', 'GuruController@update');
-  Route::get('/backend/guru/{id}/delete', 'GuruController@delete');
-  
-  Route::get('/backend/guru/pendidikan', 'PendidikanController@index');
-  Route::post('/backend/guru/pendidikan/create', 'PendidikanController@create');
-  Route::get('/backend/guru/pendidikan/{id}/edit', 'PendidikanController@edit');
-  Route::post('/backend/guru/pendidikan/{id}/update', 'PendidikanController@update');
-  Route::get('/backend/guru/pendidikan/{id}/delete', 'PendidikanController@delete');
-  
-  Route::get('/backend/guru/status', 'StatusController@index');
-  Route::post('/backend/guru/status/create', 'StatusController@create');
-  Route::get('/backend/guru/status/{id}/edit', 'StatusController@edit');
-  Route::post('/backend/guru/status/{id}/update', 'StatusController@update');
-  Route::get('/backend/guru/status/{id}/delete', 'StatusController@delete');
-  
-  Route::get('/backend/studi', 'StudiController@index');
-  Route::post('/backend/studi/create', 'StudiController@create');
-  Route::get('/backend/studi/{id}/edit', 'StudiController@edit');
-  Route::post('/backend/studi/{id}/update', 'StudiController@update');
-  Route::get('/backend/studi/{id}/delete', 'StudiController@delete');
-  
-  Route::get('/backend/profil/kepala', 'KepalaController@index');
-  Route::post('/backend/profil/kepala/{id}/update', 'KepalaController@update');
-  Route::post('/backend/profil/kepala/create', 'KepalaController@create');
-  
-  Route::get('/backend/profil/tentang', 'TentangController@index');
-  Route::post('/backend/profil/tentang/{id}/update', 'TentangController@update');
-  Route::post('/backend/profil/tentang/create', 'TentangController@create');
-  
-  Route::get('/backend/profil/identitas', 'IdentitasController@index');
-  Route::post('/backend/profil/identitas/{id}/update', 'IdentitasController@update');
-  Route::post('/backend/profil/identitas/create', 'IdentitasController@create');
-  
-  Route::get('/backend/profil/misi', 'MisiController@index');
-  Route::post('/backend/profil/misi/{id}/update', 'MisiController@update');
-  Route::post('/backend/profil/misi/create', 'MisiController@create');
-  Route::get('/backend/profil/struktur', 'StrukturController@index');
-  Route::post('/backend/profil/struktur/{id}/update', 'StrukturController@update');
-  Route::post('/backend/profil/struktur/create', 'StrukturController@create');
-  
-  
-  Route::get('/guru', 'Frontend\GuruController@index');
-  Route::get('/guru/detail/{id}', 'Frontend\GuruController@detail');
-  Route::get('/guru/status', 'Frontend\GuruController@status');
-  Route::get('/guru/pendidikan', 'Frontend\GuruController@pendidikan');
-  Route::get('/pembelajaran', 'Frontend\PembelajaranController@pembelajaran');
-  Route::get('/profil/sambutan', 'Frontend\ProfilController@sambutan');
-  Route::get('/profil/tentang', 'Frontend\ProfilController@tentang');
-  Route::get('/profil/misi', 'Frontend\ProfilController@misi');
-  Route::get('/profil/identitas', 'Frontend\ProfilController@identitas');
-  Route::get('/profil/identitas', 'Frontend\ProfilController@identitas');
-  Route::get('/profil/organisasi', 'Frontend\ProfilController@organisasi');
-  Route::get('/profil/sarana', 'Frontend\ProfilController@sarana');
-  Route::get('/pengumuman', 'Frontend\PengumumanController@pengumuman');
-    
-});
-Auth::routes();
+Route::get('/dashboard','AdminController@dashboard');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/backend/pembelajaran', 'PembelajaranController@index');
+Route::post('/backend/pembelajaran/create', 'PembelajaranController@create');
+Route::get('/backend/pembelajaran/{id}/edit', 'PembelajaranController@edit');
+Route::post('/backend/pembelajaran/{id}/update', 'PembelajaranController@update');
+Route::get('/backend/pembelajaran/{id}/delete', 'PembelajaranController@delete');
+
+Route::get('/backend/pengumuman', 'PengumumanController@index');
+Route::post('/backend/pengumuman/create', 'PengumumanController@create');
+Route::get('/backend/pengumuman/{id}/edit', 'PengumumanController@edit');
+Route::post('/backend/pengumuman/{id}/update', 'PengumumanController@update');
+Route::get('/backend/pengumuman/{id}/delete', 'PengumumanController@delete');
+
+Route::get('/backend/kegiatan', 'KegiatanController@index');
+Route::post('/backend/kegiatan/create', 'KegiatanController@create');
+Route::get('/backend/kegiatan/{id}/edit', 'KegiatanController@edit');
+Route::post('/backend/kegiatan/{id}/update', 'KegiatanController@update');
+Route::get('/backend/kegiatan/{id}/delete', 'KegiatanController@delete');
+
+Route::get('/backend/galeri', 'GaleriController@index');
+Route::post('/backend/galeri/create', 'GaleriController@create');
+Route::get('/backend/galeri/{id}/edit', 'GaleriController@edit');
+Route::post('/backend/galeri/{id}/update', 'GaleriController@update');
+Route::get('/backend/galeri/{id}/delete', 'GaleriController@delete');
+
+Route::get('/backend/profil/sarana', 'SaranaController@index');
+Route::post('/backend/profil/sarana/create', 'SaranaController@create');
+Route::get('/backend/profil/sarana/{id}/edit', 'SaranaController@edit');
+Route::post('/backend/profil/sarana/{id}/update', 'SaranaController@update');
+Route::get('/backend/profil/sarana/{id}/delete', 'SaranaController@delete');
+
+Route::get('/backend/guru', 'GuruController@index');
+Route::post('/backend/guru/create', 'GuruController@create');
+Route::get('/backend/guru/{id}/edit', 'GuruController@edit');
+Route::post('/backend/guru/{id}/update', 'GuruController@update');
+Route::get('/backend/guru/{id}/delete', 'GuruController@delete');
+
+Route::get('/backend/guru/pendidikan', 'PendidikanController@index');
+Route::post('/backend/guru/pendidikan/create', 'PendidikanController@create');
+Route::get('/backend/guru/pendidikan/{id}/edit', 'PendidikanController@edit');
+Route::post('/backend/guru/pendidikan/{id}/update', 'PendidikanController@update');
+Route::get('/backend/guru/pendidikan/{id}/delete', 'PendidikanController@delete');
+
+Route::get('/backend/guru/status', 'StatusController@index');
+Route::post('/backend/guru/status/create', 'StatusController@create');
+Route::get('/backend/guru/status/{id}/edit', 'StatusController@edit');
+Route::post('/backend/guru/status/{id}/update', 'StatusController@update');
+Route::get('/backend/guru/status/{id}/delete', 'StatusController@delete');
+
+Route::get('/backend/studi', 'StudiController@index');
+Route::post('/backend/studi/create', 'StudiController@create');
+Route::get('/backend/studi/{id}/edit', 'StudiController@edit');
+Route::post('/backend/studi/{id}/update', 'StudiController@update');
+Route::get('/backend/studi/{id}/delete', 'StudiController@delete');
+
+Route::get('/backend/profil/kepala', 'KepalaController@index');
+Route::post('/backend/profil/kepala/{id}/update', 'KepalaController@update');
+Route::post('/backend/profil/kepala/create', 'KepalaController@create');
+
+Route::get('/backend/profil/tentang', 'TentangController@index');
+Route::post('/backend/profil/tentang/{id}/update', 'TentangController@update');
+Route::post('/backend/profil/tentang/create', 'TentangController@create');
+
+Route::get('/backend/profil/identitas', 'IdentitasController@index');
+Route::post('/backend/profil/identitas/{id}/update', 'IdentitasController@update');
+Route::post('/backend/profil/identitas/create', 'IdentitasController@create');
+
+Route::get('/backend/profil/misi', 'MisiController@index');
+Route::post('/backend/profil/misi/{id}/update', 'MisiController@update');
+Route::post('/backend/profil/misi/create', 'MisiController@create');
+Route::get('/backend/profil/struktur', 'StrukturController@index');
+Route::post('/backend/profil/struktur/{id}/update', 'StrukturController@update');
+Route::post('/backend/profil/struktur/create', 'StrukturController@create');
+
+
+Route::get('/guru', 'Frontend\GuruController@index');
+Route::get('/guru/detail/{id}', 'Frontend\GuruController@detail');
+Route::get('/guru/status', 'Frontend\GuruController@status');
+Route::get('/guru/pendidikan', 'Frontend\GuruController@pendidikan');
+Route::get('/pembelajaran', 'Frontend\PembelajaranController@pembelajaran');
+Route::get('/profil/sambutan', 'Frontend\ProfilController@sambutan');
+Route::get('/profil/tentang', 'Frontend\ProfilController@tentang');
+Route::get('/profil/misi', 'Frontend\ProfilController@misi');
+Route::get('/profil/identitas', 'Frontend\ProfilController@identitas');
+Route::get('/profil/identitas', 'Frontend\ProfilController@identitas');
+Route::get('/profil/organisasi', 'Frontend\ProfilController@organisasi');
+Route::get('/profil/sarana', 'Frontend\ProfilController@sarana');
+Route::get('/pengumuman', 'Frontend\PengumumanController@pengumuman');
 
