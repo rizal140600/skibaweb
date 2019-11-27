0d1058927118ffe0956b6d3f2d60bd40e55f1c74
rizal140600 <rizal14ali06@gmail.com>
2019-11-27 19:17:24 +0700

    ayo

--- app/Http/Controllers/Frontend/PengumumanController.php
+++ app/Http/Controllers/Frontend/PengumumanController.php
@@ -10,9 +10,13 @@ class PengumumanController extends Controller
     public function pengumuman()
     {
         $identitas = \App\Identitas::first();
+        $data_pembelajaran = \App\Pembelajaran::all()->sortByDesc('updated_at');
+        $data_galeri = \App\Galeri::all()->sortByDesc('updated_at');
         $data_pengumuman = \App\Pengumuman::paginate(10);
         return view('frontend.pengumuman.pengumuman',[
             'identitas' => $identitas,
+            'data_pembelajaran' => $data_pembelajaran,
+            'data_galeri' => $data_galeri,
             'data_pengumuman' => $data_pengumuman
         ]);
     }
--- resources/views/frontend/pengumuman/pengumuman.blade.php
+++ resources/views/frontend/pengumuman/pengumuman.blade.php
@@ -15,7 +15,7 @@
 				</div>
             </div>
         </section>
-<div class="container">
+{{-- <div class="container">
   <div class="section-top-border">
     <h3 class="mb-30 title_color">Tabel Pengumuman</h3>
     <div class="card-body table-responsive p-0">
@@ -46,10 +46,83 @@
                 @endforeach
                 </tbody>
             </table>
-            <div class="container ml-3">
-                  {{$data_pengumuman->links()}}
-                </div>
+            
             </div>
   </div>
-</div>
+</div> --}}
+<section class="blog_area pt-5 pb-5">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-8 pt-5" style="background-color: #f9f9ff">
+                                <div class="blog_left_sidebar">
+                                  <div id="accordion">
+                                    @foreach ($data_pengumuman as $pengumuman)
+                                    <div class="card">
+                                      <div class="card-header" id="headingOne">
+                                        <h5 class="mb-0">
+                                          <a class="d-inline-block h4 collapse-link" href="/pengumuman/detail/{{$pengumuman->id}}">
+                                            {{$pengumuman->judul_pengumuman}}
+                                          </a>
+                                          <hr class="p-0 m-0">
+                                          <p class="d-inline-block mt-2"><i class="far fa-calendar-alt mr-2"></i>{{ \Carbon\Carbon::parse($pengumuman->updated_at)->format('d M Y')}}</p>
+                                          <button class="btn float-right btn-link" data-toggle="collapse" data-target="#pengumuman{{$pengumuman->id}}" aria-expanded="true" aria-controls="pengumuman{{$pengumuman->id}}">
+                                              <i class="fas fa-1x mt-0 fa-sort-down"></i>
+                                          </button>
+                                        </h5>
+                                      </div>
+
+                                      <div id="pengumuman{{$pengumuman->id}}" class="collapse " aria-labelledby="headingOne" data-parent="#accordion">
+                                        <div class="card-body">
+                                          {!!$pengumuman->isi_pengumuman!!}
+                                        </div>
+                                      </div>
+                                    </div>
+                                    @endforeach
+                                  </div>
+                                </div>
+                                <div class="container ml-3 mt-5">
+                                  {{$data_pengumuman->links()}}
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="blog_right_sidebar">
+                                    <aside class="single_sidebar_widget popular_post_widget">
+                                        <h3 class="widget_title">Pembelajaran</h3>
+                                        <?php $count = 0; ?>
+                                        @foreach ($data_pembelajaran as $pembelajaran)
+                                        <?php if($count == 5) break; ?>
+                                        <div class="media post_item">
+                                            <div class="media-body">
+                                                <a href="{{$pembelajaran->link}}"><h3>{{$pembelajaran->nama_file}}</h3></a>
+                                            </div>
+                                        </div>
+                                        <?php $count++; ?>
+                                        @endforeach
+                                        <div class="br"></div>
+                                    </aside>
+                                    <aside class="single_sidebar_widget post_category_widget">
+                                        <h4 class="widget_title">Pengumuman</h4>
+                                        <div class="row courses_inner">
+                                        <div class="col-lg-12">
+                                          <div class="grid_inner roww">
+                                            <?php $count = 0; ?>
+                                            @foreach ($data_galeri as $galeri)
+                                            <?php if($count == 10) break; ?>
+                                            <div class="kolom grid_item">
+                                              <div class="courses_item">
+                                                <img title"{{$galeri->judul_gambar}}" src="{{ asset("/storage/galeri/" . $galeri->gambar) }}" style="width:100%">
+                                                  </div>
+                                                </div>
+                                                <?php $count++; ?>
+                                              @endforeach
+                                              </div>
+                                            </div>
+                                          </div>
+                                        <div class="br"></div>
+                                    </aside>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section> 
 @endsection
